efb86ab6289491b1346f913f1e6f88f1eddbf80e
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-08-04 14:26:33 +0200

    add install and publish commands and jobs

---
+++ src/scripts/npm-publish.sh
@@ -0,0 +1,19 @@
+Publish() {
+    export PACKAGE_NAME=$(node -p "require('./package.json').name")
+    export LOCAL_VERSION=$(node -p "require('./package.json').version")
+    export PUBLISHED_VERSION=$(npm view $PACKAGE_NAME versions --json | jq -r '.[-1]')
+    if [[ $LOCAL_VERSION != $PUBLISHED_VERSION ]]; then
+        echo "Version $LOCAL_VERSION found unpublished. Publishing it to NPM."
+        npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
+        npm $PARAM_OVERRIDE_COMMAND
+    elif
+        echo "Version $LOCAL_VERSION is already published."
+    fi
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    Publish
+fi
