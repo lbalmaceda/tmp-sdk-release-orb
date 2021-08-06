9f06da609eb58efd3c89ee12fa1771d1e4089281
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-08-06 18:24:46 +0200

    improve commands and jobs. add support for yarn.

--- src/scripts/publish-to-npm.sh
+++ src/scripts/publish-to-npm.sh
@@ -2,11 +2,22 @@ Publish() {
     PACKAGE_NAME=$(node -p "require('./package.json').name")
     LOCAL_VERSION=$(node -p "require('./package.json').version")
     PUBLISHED_VERSION=$(npm view "$PACKAGE_NAME" versions --json | jq -r '.[-1]')
-    
+
     if [[ $LOCAL_VERSION != "$PUBLISHED_VERSION" ]]; then
-        echo "Version $LOCAL_VERSION found unpublished. Publishing it to NPM."
+        echo "Version $LOCAL_VERSION found unpublished. Publishing it to npm."
+        # Configuration from command below will be picked up by yarn as well
         npm config set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
-        npm "$PARAM_OVERRIDE_COMMAND"
+
+        if [[ -n "$PUBLISH_COMMAND" ]]; then
+            echo "Running override publish command:"
+            $($PUBLISH_COMMAND)
+        elif [[ $PKG_MANAGER == 'npm' ]]; then
+            echo "Publishing using npm:"
+            npm publish
+        else
+            echo "Publishing using yarn:"
+            yarn publish
+        fi
     else
         echo "Version $LOCAL_VERSION is already published."
     fi
