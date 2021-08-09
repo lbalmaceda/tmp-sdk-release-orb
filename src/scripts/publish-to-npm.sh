d1b8cd7e11e85d3d8965e27874395113b3f2443d
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-08-09 11:42:37 +0200

    use variable indirection to read env from variable name

--- src/scripts/publish-to-npm.sh
+++ src/scripts/publish-to-npm.sh
@@ -1,21 +1,28 @@
 Publish() {
+    NPM_TOKEN="${!NPM_TOKEN_KEY}"
+    # Check if $NPM_TOKEN is defined
+    if [[ -z "$NPM_TOKEN" ]]; then
+        echo "ERROR: The '$NPM_TOKEN_KEY' variable must be set to an Npm Automation Token."
+        exit 1
+    fi
+
     PACKAGE_NAME=$(node -p "require('./package.json').name")
     LOCAL_VERSION=$(node -p "require('./package.json').version")
     PUBLISHED_VERSION=$(npm view "$PACKAGE_NAME" versions --json | jq -r '.[-1]')
 
     if [[ $LOCAL_VERSION != "$PUBLISHED_VERSION" ]]; then
-        echo "Version $LOCAL_VERSION found unpublished. Publishing it to npm."
+        echo "Version $LOCAL_VERSION found unpublished. Publishing it to Npm."
         # Configuration from command below will be picked up by yarn as well
         npm config set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
-
+        
         if [[ -n "$PUBLISH_COMMAND" ]]; then
             echo "Running override publish command:"
             $($PUBLISH_COMMAND)
         elif [[ $PKG_MANAGER == 'npm' ]]; then
-            echo "Publishing using npm:"
+            echo "Publishing using Npm:"
             npm publish
         else
-            echo "Publishing using yarn:"
+            echo "Publishing using Yarn:"
             yarn publish
         fi
     else
