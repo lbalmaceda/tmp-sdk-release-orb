c0926eb05cccab4e319b8bb454978d1251115050
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-08-23 14:32:19 +0200

    remove bash test templates

--- src/scripts/publish-to-npm.sh
+++ src/scripts/publish-to-npm.sh
@@ -29,10 +29,3 @@ Publish() {
         echo "Version $LOCAL_VERSION is already published."
     fi
 }
-
-# Will not run if sourced for bats-core tests.
-# View src/tests for more information.
-ORB_TEST_ENV="bats-core"
-if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    Publish
-fi
