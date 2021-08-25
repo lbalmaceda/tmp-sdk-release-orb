f69a90c3c9651c32b407d0829eeb182eea50e26a
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-08-25 19:33:14 +0200

    fix JSON body parameter

--- src/scripts/make-github-release.sh
+++ src/scripts/make-github-release.sh
@@ -5,19 +5,26 @@ if [[ -z "$GH_TOKEN" ]]; then
     exit 1
 fi
 
+if [[ -z "$CIRCLE_PROJECT_USERNAME" ]] || [[ -z "$CIRCLE_PROJECT_REPONAME" ]]; then
+    echo "ERROR: The variables 'CIRCLE_PROJECT_USERNAME' and/or 'CIRCLE_PROJECT_REPONAME' are not defined. Running this locally requires those variables to be explicitly set via the CLI. See https://circleci.com/docs/2.0/local-cli/#limitations-of-running-jobs-locally."
+    exit 1
+fi
+
 LOCAL_VERSION=$(node -p "require('./package.json').version")
-PREFIXED_VERSION="$TAG_PREFIX$LOCAL_VERSION"
+PREFIXED_VERSION=$TAG_PREFIX$LOCAL_VERSION
 
 if ! git show-ref --tags "$PREFIXED_VERSION"; then
     echo "Tag with name '$PREFIXED_VERSION' found unpublished."
-
-    STATUS_CODE=$(curl --silent \
+    
+    API_URL="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases"
+    STATUS_CODE=$(curl \
     --output /dev/stderr \
     --write-out "%{http_code}" \
     -H "Accept: application/json" \
     -H "Authorization: token $GH_TOKEN" \
-    -d "tag_name=$PREFIXED_VERSION" \
-    "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases")
+    -d '{"tag_name":'\""$PREFIXED_VERSION"\"'}' \
+    "$API_URL"
+    )
     
     if [ "$STATUS_CODE" -ne 200 ]; then exit "$STATUS_CODE"; fi
     
